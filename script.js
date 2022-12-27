073f5b138223275fa1ddc262d0d22441780822ab
viniciuscanutx <viniciuscanutmz@gmail.com>
2022-12-27 10:55:44 -0300

    new repository, new project

---
+++ script.js
@@ -0,0 +1,5 @@
+function abre1(){
+    var valor1 = document.getElementById("recebe").value;
+    var valorddd =  document.getElementById("selectddd1").value;
+    window.open('http://wa.me/+55' + valorddd + valor1);
+}
