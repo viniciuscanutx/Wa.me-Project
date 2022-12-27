305d6055216cca57296bbd31c6dc2d6b717c7816
viniciuscanutx <viniciuscanutmz@gmail.com>
2022-12-27 11:50:19 -0300

    fix local number problem

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function abre1(){
     var valor1 = document.getElementById("recebe").value;
+    var valor2 =  document.getElementById("selectddd1").value;
     var strNum = valor1.replace(/[^0-9]/g, '')
-    window.open('http://wa.me/+55' + strNum);
+    window.open('http://wa.me/' + valor2 + strNum);
 }
